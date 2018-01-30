2e2c095f5f5d7a92a7cf20f2843153369649ed2d
yanghai <yanghai0523@gmail.com>
2018-01-30 14:10:42 +0800

    modify err

--- src/service/Service.go
+++ src/service/Service.go
@@ -1,18 +1,17 @@
 package main
 
 import (
-	"fmt"
 	"net"
-	"os"
 	"time"
+	"github.com/yanghai23/GoLib/aterr"
 )
 
 func main() {
 	service := ":7777"
 	tcpAddr, err := net.ResolveTCPAddr("tcp4", service)
-	checkError(err)
+	aterr.CheckErr(err)
 	listener, err := net.ListenTCP("tcp", tcpAddr)
-	checkError(err)
+	aterr.CheckErr(err)
 	for {
 		conn, err := listener.Accept()
 		if err != nil {
@@ -27,9 +26,4 @@ func handleClient(conn net.Conn) {
 	conn.Write([]byte(daytime)) // don't care about return value
 	              // we're finished with this client
 }
-func checkError(err error) {
-	if err != nil {
-		fmt.Fprintf(os.Stderr, "Fatal error: %s", err.Error())
-		os.Exit(1)
-	}
-}
+
